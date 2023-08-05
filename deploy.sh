e38977efc068019cb262c5501756a525b145b9b4
Siggi Simonarson <siggi@buildbuddy.io>
2023-08-04 18:59:56 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-gcloud services enable run.googleapis.com --project=$GCP_PROJECT
+external/gcloud/gcloud services enable run.googleapis.com --project=$GCP_PROJECT
 
-gcloud run deploy test-$RANDOM --image=gcr.io/flame-public/buildbuddy-app-onprem:latest --port=8080 --region=us-west1 --project=$GCP_PROJECT --allow-unauthenticated
+external/gcloud/gcloud run deploy test-$RANDOM --image=gcr.io/flame-public/buildbuddy-app-onprem:latest --port=8080 --region=us-west1 --project=$GCP_PROJECT --allow-unauthenticated
